0e4753bf9fb6643aca0c69d3923d354c523cbdd4
Umer <umernisar053@gmail.com>
2025-07-13 15:29:06 +0500

    okk

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
 const path = require("path");
 const {v4 : uuidv4} = require("uuid");
 const methodOverride = require("method-override");
@@ -14,10 +19,6 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname , "public")));
-app.listen(port,()=>{
-    console.log("Listening to port : 3000");
-
-});
 
 let posts = [
     {
